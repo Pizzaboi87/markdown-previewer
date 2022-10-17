97be5679a0acb6f42e4fc5045caf82518c864de2
Peter Weiser <peterweiser87@gmail.com>
2022-10-17 22:30:56 +0300

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { marked } from 'marked';
+import data from './data';
 
 function App() {
+
+  const [note, setNote] = useState(data)
+
+  const handleChange = (event) => {
+    setNote(event.target.value)
+  }
+
+  const markdown = marked(note, {breaks: true})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mt-5 title">My Markdown Previewer</h1> 
+      <div className="row page">
+        <div className="col-5" id="editorboard">
+          <h4 className="mt-5 text-center subtitle">Enter your markdown:</h4>
+          <textarea className="form-control rounded p-3" id="editor" value={note} onChange={handleChange} />
+        </div>
+        <div className="col-5" id="preview">
+          <h4 className="mt-5 text-center subtitle">Check the result:</h4>
+          <div className="preview rounded p-3" dangerouslySetInnerHTML={{__html: markdown}}/>
+        </div>
+      </div>
     </div>
   );
 }
